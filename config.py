40782630aee2962daf954bd0beecd0127397a86a
anees <aneesurrehman075@gmail.com>
2026-07-04 15:42:57 -0700

    qdrant

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ EMBEDDING_DIM = 384
 
 QDRANT_URL = os.getenv("QDRANT_URL")
 QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
-QDRANT_COLLECTION = os.getenv("QDRANT_COLLECTION", "academy-knowledge-base")
+QDRANT_COLLECTION = os.getenv("QDRANT_COLLECTION")
 TOP_K = 5
